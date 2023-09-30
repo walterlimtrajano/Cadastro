5e39327d8c16d10350a73b8ddef73465f1c3f0d7
walterlimtrajano <walterlimyounkou@gmail.com>
2023-09-30 11:42:19 -0300

    primeiro commit

---
+++ cadastro/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Aluno(models.Model):
+    foto =  models.ImageField(upload_to='fotos')
+    nome = models.CharField(max_length=250)
+    idade = models.IntegerField()
+    cpf = models.CharField(max_length=11)
+    endereco =  models.CharField(max_length=255)
+    cep =  models.CharField(max_length=8)
+    email =  models.EmailField()
+    contato = models.CharField(max_length=11)
+
+    def __str__(self):
+        return self.nome
